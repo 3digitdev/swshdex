7c272e3854655a23dc9fb8be6120d764f6af52df
Max Bussiere <3digitdev@gmail.com>
2019-12-22 17:26:47 -0600

    let's merge

--- buildElm.sh
+++ buildElm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-elm make src/Main.elm --output=public/index.js
-# elm make src/Main.elm --optimize --output=public/index.js
+# elm make src/Main.elm --output=public/index.js
+elm make src/Main.elm --optimize --output=public/index.js
 elm reactor
