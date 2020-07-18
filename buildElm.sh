c9159337d8b518e41189b5894b696e69d683c446
Max Bussiere <max@3digit.dev>
2020-07-18 13:18:11 -0500

    debug

--- buildElm.sh
+++ buildElm.sh
@@ -6,5 +6,5 @@
 elm make src/Main.elm --optimize --output=public/index.js
 
 # Debug Commands
-elm make src/Main.elm --output=public/index.js
-elm reactor
+# elm make src/Main.elm --output=public/index.js
+# elm reactor
