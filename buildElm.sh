95ddbbbe8e6a3b3dcfeb6a61d012c2fa35938d4d
Max Bussiere <max@3digit.dev>
2020-07-18 21:28:20 -0500

    apparently i need the js file from local.

--- buildElm.sh
+++ buildElm.sh
@@ -3,7 +3,7 @@
 # Standard building
 
 # Compile Elm
-elm make src/Main.elm --optimize --output=public/elm.311.js
+elm make src/Main.elm --optimize --output=public/elm.312.js
 
 # Debug Commands
 # elm make src/Main.elm --output=public/elm.js
